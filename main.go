cc0a2e44b69d66310d8873abb42bf604c278bbbf
Paul Forman <pforman@rocketjets.com>
2016-04-10 00:10:44 -0600

    Cleanup, verify region, `go fmt`
    Cleaned up flag usage, added a verifyRegion func to detect the region in
    any way possible.
    
    Ran `go fmt`, which creates tons of whitespace differy

--- main.go
+++ main.go
@@ -1,169 +1,169 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-  "time"
-  "flag"
+	"time"
 
-  "github.com/aws/aws-sdk-go/aws"
-  "github.com/aws/aws-sdk-go/aws/session"
-  "github.com/aws/aws-sdk-go/service/ec2"
-  //"github.com/jessevdk/go-flags"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	//"github.com/jessevdk/go-flags"
 )
 
-func snap (session *session.Session, volumeId string, desc string) (string, error) {
-  svc := ec2.New(session)
+func snap(session *session.Session, volumeId string, desc string) (string, error) {
+	svc := ec2.New(session)
 
-  params := &ec2.CreateSnapshotInput{
-    VolumeId:    aws.String(volumeId),
-    Description: aws.String(desc),
-  }
+	params := &ec2.CreateSnapshotInput{
+		VolumeId:    aws.String(volumeId),
+		Description: aws.String(desc),
+	}
 
-  resp,err := svc.CreateSnapshot(params)
-  if err != nil {
-    // Print the error, cast err to awserr.Error to get the Code and
-    // Message from an error.
-    fmt.Println(err.Error())
-    return "", err
-  }
+	resp, err := svc.CreateSnapshot(params)
+	if err != nil {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		fmt.Println(err.Error())
+		return "", err
+	}
 
-  // Pretty-print the response data.
-  return *resp.SnapshotId, nil
+	// Pretty-print the response data.
+	return *resp.SnapshotId, nil
 
 }
 
-func tagSnapshot(session *session.Session, snapId string,expires string) (error) {
-  svc := ec2.New(session)
+func tagSnapshot(session *session.Session, snapId string, expires string) error {
+	svc := ec2.New(session)
 
-  params := &ec2.CreateTagsInput{
-    Resources: []*string{
-      aws.String(snapId),
-    },
-    Tags: []*ec2.Tag{
-      {
-        Key:   aws.String("Expires"),
-        Value: aws.String(expires),
-      },
-      // More values...
-    },
-  }
-  resp, err := svc.CreateTags(params)
+	params := &ec2.CreateTagsInput{
+		Resources: []*string{
+			aws.String(snapId),
+		},
+		Tags: []*ec2.Tag{
+			{
+				Key:   aws.String("Expires"),
+				Value: aws.String(expires),
+			},
+			// More values...
+		},
+	}
+	resp, err := svc.CreateTags(params)
 
-  if err != nil {
-    // Print the error, cast err to awserr.Error to get the Code and
-    // Message from an error.
-    fmt.Println(err.Error())
-    return err
-  }
+	if err != nil {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		fmt.Println(err.Error())
+		return err
+	}
 
-  // Pretty-print the response data.
-  fmt.Println(resp)
-  return nil
+	// Pretty-print the response data.
+	fmt.Println(resp)
+	return nil
 }
 
-func findVolumeId (session *session.Session, device string , instance string) (string, error) {
+func findVolumeId(session *session.Session, device string, instance string) (string, error) {
 
-  svc := ec2.New(session)
+	svc := ec2.New(session)
 
-  println("fVID: working with device", device,"instance", instance)
-  params := &ec2.DescribeVolumesInput{
-    DryRun: aws.Bool(false),
-    Filters: []*ec2.Filter{
-      {
-        Name: aws.String("attachment.instance-id"),
-        Values: []*string{
-          aws.String(instance),
-        },
-      },
-      {
-        Name: aws.String("attachment.device"),
-        Values: []*string{
-          aws.String(device),
-        },
-      },
-    },
-  }
-  resp, err := svc.DescribeVolumes(params)
+	println("fVID: working with device", device, "instance", instance)
+	params := &ec2.DescribeVolumesInput{
+		DryRun: aws.Bool(false),
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("attachment.instance-id"),
+				Values: []*string{
+					aws.String(instance),
+				},
+			},
+			{
+				Name: aws.String("attachment.device"),
+				Values: []*string{
+					aws.String(device),
+				},
+			},
+		},
+	}
+	resp, err := svc.DescribeVolumes(params)
 
-  if err != nil {
-    return "", err
-  }
+	if err != nil {
+		return "", err
+	}
 
-  if len(resp.Volumes) > 0 {
-    // is there ever some case when we might get back 2?
-    return *resp.Volumes[0].VolumeId, nil
-  }
-  return "", fmt.Errorf("unable to find volume ID for device %s on instance %s", device, instance)
+	if len(resp.Volumes) > 0 {
+		// is there ever some case when we might get back 2?
+		return *resp.Volumes[0].VolumeId, nil
+	}
+	return "", fmt.Errorf("unable to find volume ID for device %s on instance %s", device, instance)
 }
 
 func main() {
 
-  //var noop = flag.Bool("noop", true, "test operation, no action")
-  var expires = flag.Int("expires", 1, "sets the expiration time in days")
-  //var instance = flag.String("instance", "i-6ee11663", "instance-id")
-  var instparam = flag.String("instance", "", "instance-id")
-  var region = flag.String("region", "us-west-2", "region of instance")
-  flag.Parse()
+	var instance, region, mount, device string
 
-  mount := flag.Arg(0)
-  instance := *instparam
+	//var noop = flag.Bool("noop", true, "test operation, no action")
+	var expires = flag.Int("expires", 1, "sets the expiration time in days")
+	//var instance = flag.String("instance", "i-6ee11663", "instance-id")
+	flag.StringVar(&instance, "instance", "", "instance-id (for remote snaps only)")
+	flag.StringVar(&region, "region", "", "region of instance (for remote snaps only)")
+	flag.Parse()
 
-  if flag.Arg(1) != "" {
-    println("error: multiple mounts provided")
-    flag.Usage()
-    os.Exit(1)
-  }
+	mount = flag.Arg(0)
 
-  if mount == ""  {
-    println ("error: must provide one mount to snapshot (example:  / )")
-    flag.Usage()
-    os.Exit(1)
-  }
+	if flag.Arg(1) != "" {
+		println("error: multiple mounts provided")
+		flag.Usage()
+		os.Exit(1)
+	}
 
-  device, err := findDeviceFromMount(mount)
-  if err != nil {
-    fmt.Printf("error determining device for mount %s: %s\n", mount, err.Error())
-    os.Exit(1)
-  }
+	if mount == "" {
+		println("error: must provide one mount to snapshot (example:  / )")
+		flag.Usage()
+		os.Exit(1)
+	}
 
-  println ("main: found device", device)
+	// If we don't have a region, we're in for trouble
+	region = verifyRegion(region)
+	session := session.New(&aws.Config{Region: aws.String(region)})
 
-  session := session.New(&aws.Config{Region: aws.String(*region)})
+	device, err := findDeviceFromMount(mount)
+	if err != nil {
+		fmt.Printf("error determining device for mount %s: %s\n", mount, err.Error())
+		os.Exit(1)
+	}
 
-  startingTime := time.Now().UTC()
-  expireTag := fmt.Sprintf ("%v", startingTime.AddDate(0,0,*expires).Round(time.Second))
+	println("main: found device", device)
 
+	startingTime := time.Now().UTC()
+	expireTag := fmt.Sprintf("%v", startingTime.AddDate(0, 0, *expires).Round(time.Second))
 
-  println ("here we go")
+	instance, err = verifyInstance(session, instance)
+	if err != nil {
+		fmt.Printf("error finding instance (found '%s'): %s\n", instance, err.Error())
+		os.Exit(1)
+	}
 
-  instance, err = verifyInstance(session, instance)
-  if err != nil {
-    fmt.Printf("error finding instance (found '%s'): %s\n", instance, err.Error())
-    os.Exit(1)
-  }
+	volumeId, err := findVolumeId(session, device, instance)
+	if err != nil {
+		fmt.Printf("error finding volume id for device %s: %s\n", device, err.Error())
+		os.Exit(1)
+	}
 
-  volumeId, err := findVolumeId(session, device, instance)
-  if err != nil {
-    fmt.Printf("error finding volume id for device %s: %s\n", device, err.Error())
-    os.Exit(1)
-  }
+	// old autosnap uses hostname instead of instance-id
+	// maybe we should find that...
+	snapDesc := fmt.Sprintf("ebs-snap %s:%s:%s", instance, device, mount)
+	snapId, err := snap(session, volumeId, snapDesc)
+	if err != nil {
+		fmt.Printf("error creating snapshot for volume %s: %s\n", volumeId, err.Error())
+		os.Exit(1)
+	}
+	println("OMG, snapped", snapId)
+	err = tagSnapshot(session, snapId, expireTag)
+	if err != nil {
+		println("error in tagging:", err)
+		// delete here.
+	}
 
-  // old autosnap uses hostname instead of instance-id
-  // maybe we should find that...
-  snapDesc := fmt.Sprintf("ebs-snap %s:%s:%s", instance, device, mount)
-  snapId, err := snap(session, volumeId, snapDesc)
-  if err != nil {
-    fmt.Printf("error creating snapshot for volume %s: %s\n", volumeId, err.Error())
-    os.Exit(1)
-  }
-  println ("OMG, snapped",snapId)
-  err = tagSnapshot(session, snapId, expireTag)
-  if err != nil {
-    println ("error in tagging:", err)
-    // delete here.
-  }
-
-  os.Exit(0)
+	os.Exit(0)
 
 }
--- util.go
+++ util.go
@@ -1,88 +1,112 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
-  "regexp"
-  "runtime"
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"runtime"
 
-  "github.com/aws/aws-sdk-go/aws"
-  "github.com/aws/aws-sdk-go/aws/session"
-  "github.com/aws/aws-sdk-go/service/ec2"
-  "github.com/aws/aws-sdk-go/aws/ec2metadata"
-
-
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
-func findDeviceFromMount (mount string) (string, error) {
+func findDeviceFromMount(mount string) (string, error) {
 
-  // stub for Mac devel
-  if runtime.GOOS != "linux" {
-    println("*** Not on Linux?  You're going to have a bad day.")
-    println("returning static device /dev/xvda for testing only.")
-    return "/dev/xvda", nil
-  }
+	// stub for Mac devel
+	if runtime.GOOS != "linux" {
+		println("*** Not on Linux?  You're going to have a bad day.")
+		println("returning static device /dev/xvda for testing only.")
+		return "/dev/xvda", nil
+	}
 
-  var device string = ""
-  // Serious Linux-only stuff happening here...
-  // If we can't read /proc/mounts, nothing good can happen.  Get out.
-  file :=  "/proc/mounts"
-  _, err := os.Stat(file)
-  if err != nil {
-    fmt.Printf("Cannot stat file %s: %s", file, err.Error())
-    os.Exit(1)
-  }
-  v, err := os.Open(file)
-  if err != nil {
-    fmt.Printf("Failed to open %s: %v", file, err)
-    os.Exit(1)
-  }
+	var device string = ""
+	// Serious Linux-only stuff happening here...
+	// If we can't read /proc/mounts, nothing good can happen.  Get out.
+	file := "/proc/mounts"
+	_, err := os.Stat(file)
+	if err != nil {
+		fmt.Printf("Cannot stat file %s: %s", file, err.Error())
+		os.Exit(1)
+	}
+	v, err := os.Open(file)
+	if err != nil {
+		fmt.Printf("Failed to open %s: %v", file, err)
+		os.Exit(1)
+	}
 
-  scanner := bufio.NewScanner(v)
-  // leading slash on device to avoid matching things like "rootfs"
-  r := regexp.MustCompile(`^(?P<device>/\S+) (?P<mount>\S+) `)
-  for scanner.Scan() {
-    result := r.FindStringSubmatch(scanner.Text())
-    if len(result) > 1 {
-      if result[2] == mount {
-        println ("fDFM: found device", result[1], " mount ", result[2])
-        device = result[1]
-      }
-    }
-  }
-  if device == "" {
-    return device, fmt.Errorf("No device found for mount %s", mount)
-  }
-  return device, nil
+	scanner := bufio.NewScanner(v)
+	// leading slash on device to avoid matching things like "rootfs"
+	r := regexp.MustCompile(`^(?P<device>/\S+) (?P<mount>\S+) `)
+	for scanner.Scan() {
+		result := r.FindStringSubmatch(scanner.Text())
+		if len(result) > 1 {
+			if result[2] == mount {
+				println("fDFM: found device", result[1], " mount ", result[2])
+				device = result[1]
+			}
+		}
+	}
+	if device == "" {
+		return device, fmt.Errorf("No device found for mount %s", mount)
+	}
+	return device, nil
 }
 
-func verifyInstance (session *session.Session, instance string) (string, error) {
-  svc := ec2.New(session)
-  mdsvc := ec2metadata.New(session)
-  // if there's no instance specified, go look it up in metadata
-  if instance == "" {
-    println ("No instance-id specified, attempting to use local instance")
-    i, err := mdsvc.GetMetadata("instance-id")
-    if err != nil {
-      return "", err
-    }
-    println ("cool, returning instance",i,"hyuk-hyuk")
-    return i, nil
-  } else {
-    // go verify the instance exists...
-    params := &ec2.DescribeInstancesInput{
-      InstanceIds: []*string{
-        aws.String(instance),
-      },
-    }
-    _, err := svc.DescribeInstances(params)
-    if err != nil {
-      return "", err
-    }
-    return instance, nil
-  }
+func verifyInstance(session *session.Session, instance string) (string, error) {
+	svc := ec2.New(session)
+	mdsvc := ec2metadata.New(session)
+	// if there's no instance specified, go look it up in metadata
+	if instance == "" {
+		println("No instance-id specified, attempting to use local instance")
+		i, err := mdsvc.GetMetadata("instance-id")
+		if err != nil {
+			println("Cannot detect instance-id, exiting.")
+			println("Provide an instance-id with '-instance' for remote operation")
+			os.Exit(1)
+		}
+		return i, nil
+	} else {
+		// go verify the instance exists...
+		params := &ec2.DescribeInstancesInput{
+			InstanceIds: []*string{
+				aws.String(instance),
+			},
+		}
+		_, err := svc.DescribeInstances(params)
+		if err != nil {
+			return "", err
+		}
+		return instance, nil
+	}
 
-  // should never get here
-  return "", fmt.Errorf("unknown error in VerifyInstance")
+	// should never get here
+	return "", fmt.Errorf("unknown error in VerifyInstance")
+}
+
+func verifyRegion(region string) string {
+	// if we don't have a region provided, then try to look one up
+	// Check the env variable, then metadata
+	if region == "" {
+		if os.Getenv("AWS_REGION") != "" {
+			// We'll be okay, the aws-sdk can use this
+			return os.Getenv("AWS_REGION")
+		}
+
+		// We're only going to use this for metadata, since we're
+		// trying to *find* the region..
+		s := session.New()
+		svc := ec2metadata.New(s)
+		res, err := svc.Region()
+		if err != nil {
+			println("Error detecting region, exiting.")
+			println("Provide a region with '-region' or set AWS_REGION")
+			os.Exit(1)
+		}
+		return res
+	}
+	// if the user provided something, go with it.
+	return region
 }
